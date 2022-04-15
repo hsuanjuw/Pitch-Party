98a2dd6adede567f83d1f9278aee659ec62c6588
Phoebe <hsuanju.wang@gmail.com>
2022-04-15 07:35:09 -0400

    Implemented function of random generate inspirations and UI

---
+++ js/contentGenerate.js
@@ -0,0 +1,373 @@
+let contentDic = {
+  0:{
+      topic : "Use AR (Augmented Reality) for an educational experience",
+      category : "Technology",
+      inspiration : [
+        "Hololens are a pair of mixed reality smart glasses developed and manufactured by Microsoft. ",
+        "Mobile phone manufacturers like Apple are combining hardware with machine learning and cutting‑edge developer tools to enable realistic and engaging AR experiences.",
+        "Projected AR is technology that directly overlays digitals projections onto the physical world. Unlike smartphone or wearable AR, projected AR does not typically require a device to mediate and project imagery.",
+        "Mixed reality (MR) is the merging of real and virtual worlds to produce new environments and visualizations, where physical and digital objects co-exist and interact in real time."
+      ],
+      imgs : [
+        "img/inspiration/AR_Hololens.png",
+        "img/inspiration/AR_Mobile_AR.png",
+        "img/inspiration/AR_Projected_AR.png",
+        "img/inspiration/AR_Mixed_Reality.png"
+      ]
+    }, 
+  1:{
+      topic : "Design an interactive experience on water resource protection",
+      category : "Science",
+      inspiration : [
+        "Lead can contaminate drinking water via the corrosion of lead-containing plumbing materials, causing a number of health issues in humans.",
+        "In order to reduce water usage, water management techniques like the restroom fixture refitting and rainwater recovery can be implemented.",
+        "Heavy rainfall can often cause sewage to overflow into local waterways. Sewer overflow is a recurrent issue in Pittsburgh. ",
+        "A healthy watershed provides benefits to natural ecosystems, water quality, and even local economies. "      
+       ],
+      imgs : [
+        "img/inspiration/Water_Protection_Lead.png",
+        "img/inspiration/Water_Protection_Management.png",
+        "img/inspiration/Water_Protection_Sewer.png",
+        "img/inspiration/Water_Protection_Watersheds.png"
+      ]
+    },
+  2:{
+      topic : "Design an experience based on your impression of Pittsburgh",
+      category : "Technology",
+      inspiration : [
+        "Pittsburgh is the “City of Bridges” with 446 bridges.",
+        "Emoticons were invented by Scott Fahlman at Carnegie Mellon University in 1980.",
+        "The Polio vaccine was invented by Dr. Jonas Salk in Pittsburgh in 1950. ",
+        "Mister Rogers’ Neighborhood was produced by WQED in Pittsburgh. "      
+       ],
+      imgs : [
+        "img/inspiration/Pittsburgh_Bridges.png",
+        "img/inspiration/Pittsburgh_Emoticons.png",
+        "img/inspiration/Pittsburgh_Jonas_Salk.png",
+        "img/inspiration/Pittsburgh_Mister_Rogers’_Neighborhood.png"
+      ]
+    },
+  3:{
+      topic : "Use gesture technology to create an experience on climate change",
+      category : "Technology",
+      inspiration : [
+        "Kinect allows a user to move their body to control games, movies, and more. It makes users feel immersed in a simulated world, via hardware (headsets) and software.",
+        "Full Body Tracking makes virtual celebrities or VR avatars possible. It gives people a way to play the role of a digital character.",
+        "Gesture Tracking is already implemented in devices like phones. With a few hand movements, you can tell your phone to turn on, mute your alerts, or take a screenshot.",
+        "Researchers are using gesture technology to discover how body movements can impact on the potential use in Higher Education.  "      
+       ],
+      imgs : [
+        "img/inspiration/GestureTechnology_Kinect.png",
+        "img/inspiration/GestureTechnology_VRChat.jpg",
+        "img/inspiration/GestureTechnology_Samsung.png",
+        "img/inspiration/GestureTechnology_Education.png"
+      ]
+    },
+  4:{
+      topic : "Design an experience based on a personal hobby",
+      category : "Diversity",
+      inspiration : [
+        "Cooking",
+        "Hiking",
+        "Gardening",
+        "Reading"      
+       ],
+      imgs : [
+        "img/inspiration/Hobbies_Cooking.png",
+        "img/inspiration/Hobbies_Hiking.png",
+        "img/inspiration/Hobbies_Gardening.png",
+        "img/inspiration/Hobbies_Reading.png"
+      ]
+    },
+  5:{
+      topic : "Use a mobile app in an unconventional way",
+      category : "Technology",
+      inspiration : [
+        "Both Android devices and iPhones incorporate haptic feedback. Haptics allow for users to receive touch feedback, typically in the form of vibration.",
+        "The Magnifier is an iPhone accessibility tool found in the Control Center. Its features include magnification, color filters, and displaying your distance from another person.",
+        "Measure is an app installed on most iPhones. It allows users to automatically or manually measure objects and distances.",
+        "Gesture Tracking is already implemented in devices like phones. With a few hand movements, you can tell your phone to turn on, mute your alerts, or take a screenshot."      
+       ],
+      imgs : [
+        "img/inspiration/Mobile_Haptics.png",
+        "img/inspiration/Mobile_Magnifier.png",
+        "img/inspiration/Mobile_Measure.png",
+        "img/inspiration/GestureTechnology_Samsung.png"
+      ]
+    },
+  6:{
+      topic : "Highlight women’s contributions to science and technology",
+      category : "Diversity",
+      inspiration : [
+        "Chien-Shiung Wu (1912 - 1997) was a Chinese American physicist who worked on the Manhattan Project.",
+        "Katherine Johnson (1918 - 2020) was an African American mathematician who worked on the space program at NASA.",
+        "Marie Curie (1867-1934) was a Polish French physicist and two time Nobel Prize winner who studied radioactivity.",
+        "Youyou Tu (1930 - now) is a Chinese scientist and Nobel Prize winner who discovered a novel Malaria therapy."      
+       ],
+      imgs : [
+        "img/inspiration/Women_Chien-Shiung_Wu.png",
+        "img/inspiration/Women_Katherine_Johnson.png",
+        "img/inspiration/Women_Marie_Curie.png",
+        "img/inspiration/Women_Youyou_Tu.png"
+      ]
+    },
+  7:{
+      topic : "Highlight minorities’ contributions to science and technology",
+      category : "Diversity",
+      inspiration : [
+        "George Washington Carver (1864–1943) is an African American scientist who studied agricultural production and promoted new farming methods.",
+        "Mario J Molina (1943 - 2020) was a Mexican environmental chemist and Nobel Prize winner who studied Earth’s ozone layer.",
+        "Roger Y Tsien (1952 - 2016) was a Chinese American biochemist and Nobel Prize winner who worked with fluorescent proteins.",
+        "Subrahmanyan Chandrasekhar (1910 - 1995) was an Indian American astrophysicist and Nobel Prize winner who studied the evolution of stars."      
+       ],
+      imgs : [
+        "img/inspiration/Minorities_George_Washington_Carver.png",
+        "img/inspiration/Minorities_Mario_Molina.png",
+        "img/inspiration/Minorities_Roger_Tsien.jpg",
+        "img/inspiration/Minorities_Subrahmanyan_Chandrasekhar.png"
+      ]
+    },
+  8:{
+      topic : "Highlight a scientist who you believe deserves more recognition",
+      category : "Diversity",
+      inspiration : [
+        "Chien-Shiung Wu (1912 - 1997) was a Chinese American physicist who worked on the Manhattan Project.",
+        "Katherine Johnson (1918 - 2020) was an African American mathematician who worked on the space program at NASA.",
+        "Roger Y Tsien (1952 - 2016) was a Chinese American biochemist and Nobel Prize winner who worked with fluorescent proteins.",
+        "Mario J Molina (1943 - 2020) was a Mexican environmental chemist and Nobel Prize winner who studied Earth’s ozone layer."      
+       ],
+      imgs : [
+        "img/inspiration/Women_Chien-Shiung_Wu.png",
+        "img/inspiration/Women_Katherine_Johnson.png",
+        "img/inspiration/Minorities_Roger_Tsien.jpg",
+        "img/inspiration/Minorities_Mario_Molina.png"
+      ]
+    },
+  9:{
+      topic : "Design an experience to reflect monthly astronomy discoveries",
+      category : "Science",
+      inspiration : [
+        "Exoplanets are planets located outside of our solar systems. To date, NASA has confirmed over 5000 exoplanets.",
+        "Hubble Space Telescope was deployed by the space shuttle Discovery in 1990 and has helped scientists observe distant astronomical objects.",
+        "Rovers help explore the surface of Mars. The newest NASA rover, Perseverance, landed on Mars in February of 2021.",
+        "With the retirement of NASA’s space shuttles, NASA has partnered with private companies for future space travel missions."      
+       ],
+      imgs : [
+        "img/inspiration/Astronomy_Exoplanets.png",
+        "img/inspiration/Astronomy_Hubble.png",
+        "img/inspiration/Astronomy_Rovers.png",
+        "img/inspiration/Astronomy_Space_Travel.png"
+      ]
+    },
+  10:{
+      topic : "Combat health misinformation",
+      category : "Science",
+      inspiration : [
+        "Misinformation about cancer. For example, what treatments are effective against cancer? Do alternative health treatments work?",
+        "Misinformation about the coronavirus. For example, which treatments are effective against the coronavirus?",
+        "Teach people how to identify health misinformation. For example, is the information source credible? If the source is a research paper, is it peer reviewed?",
+        "Misinformation about vaccines. For example, the belief that vaccines cause autism."      
+       ],
+      imgs : [
+        "img/inspiration/Health_Misinformation_Cancer.png",
+        "img/inspiration/Health_Misinformation_Coronaviruses.png",
+        "img/inspiration/Health_Misinformation_Identification.png",
+        "img/inspiration/Health_Misinformation_Vaccine.png"
+      ]
+    },
+  11:{
+      topic : "Combat climate change misinformation",
+      category : "Science",
+      inspiration : [
+        "Misinformation on our ability to combat climate change. For example, the belief that it is too late to do anything about climate change.",
+        "Misinformation on the causes of climate change. For example, the belief that humans aren't contributing to climate change.",
+        "Misinformation on whether climate change is real. Some people believe there is no scientific consensus that global warming is currently occurring.",
+        "Teach people how to identify climate change misinformation. For example, is the information source credible?"      
+       ],
+      imgs : [
+        "img/inspiration/Climate_Misinformation_Ability.png",
+        "img/inspiration/Climate_Misinformation_Causes.png",
+        "img/inspiration/Climate_Misinformation_Rea.png",
+        "img/inspiration/Health_Misinformation_Identification.png"
+      ]
+    },
+  12:{
+      topic : "Create an interactive learning experience for large groups",
+      category : "Diversity",
+      inspiration : [
+        "College student groups",
+        "Elementary school field trip",
+        "Senior group tours",
+        "Tourists"      
+       ],
+      imgs : [
+        "img/inspiration/Education_Group_College.png",
+        "img/inspiration/Education_Group_Elementary_School.png",
+        "img/inspiration/Education_Group_Seniors.png",
+        "img/inspiration/Education_Group_Tourists.png"
+      ]
+    },
+  13:{
+      topic : "Create an immersive experiential event",
+      category : "Diversity",
+      inspiration : [
+        "Projected AR is technology that directly overlays digitals projections onto the physical world. Unlike smartphone or wearable AR, projected AR does not typically require a device to mediate and project imagery.",
+        "Mixed reality (MR) is the merging of real and virtual worlds to produce new environments and visualizations, where physical and digital objects co-exist and interact in real time.",
+        "VR Gloves provide hand tracking and haptic feedback to give users a more immersive experience",
+        "A 42 person moving ride uses 11- 8,000 lumen laser projectors with ultra short throw lenses to take you on a journey through the oil drilling process."      
+       ],
+      imgs : [
+        "img/inspiration/AR_Projected_AR.png",
+        "img/inspiration/AR_Mixed_Reality.png",
+        "img/inspiration/VR_VRGloves.png",
+        "img/inspiration/ImmersiveExperience_EFX_3000.png"
+      ]
+    },
+  14:{
+      topic : "Create a VR (Virtual Reality) experience on the pandemic",
+      category : "Technology",
+      inspiration : [
+        "Virtual reality (VR) is the experience where users feel immersed in a simulated world, via hardware (headsets) and software.",
+        "Oculus Quest 2 is one of the best selling VR Headsets. It aims to create virtual social spaces.",
+        "The Virtuix Omni is an omnidirectional treadmill simulator for virtual reality games and other applications. It uses a platform to simulate locomotion i.e. the motion of walking.",
+        "VR Gloves provide hand tracking and haptic feedback to give users a more immersive experience."      
+       ],
+      imgs : [
+        "img/inspiration/VR_VRHeadset.png",
+        "img/inspiration/VR_Quest2.png",
+        "img/inspiration/VR_TheVirtuixOmni.png",
+        "img/inspiration/VR_VRGloves.png"
+      ]
+    },
+  15:{
+      topic : "Combine two different technologies for an interactive experience",
+      category : "Technology"
+    }, 
+  16:{
+      topic : "Design an exhibit that can be easily updated yearly",
+      category : "Diversity"
+    }  
+}
+
+const refinements = [
+  "Understandable for non-English speakers",
+  "Includes all age groups", 
+  "Accessible for physically disabled individuals",
+  "Easy to maintain social distance",
+  "Acts as a local landmark",
+  "Can be a traveling exhibit",
+  "Requires communication with other visitors",
+  "Integrates storytelling into the experience",
+  "Lets visitors become a permanent part of the experience",
+  "Lets visitors enjoy a personalized experience", 
+  "Improves visitors’ mental health",
+  "Costs 300 dollars to visit",
+  "Requires visitors to wear sports clothes",
+  "Opens only at night",
+  "Opens for only 1 hour a day",
+  "Located on top of a mountain",
+  "Located at a public transit stop",
+  "Involves pop culture (anime, superheroes, etc.)",
+  "Requires visitors to wear Halloween costumes", 
+  "Primarily targeted towards visitors’ pets", 
+  "Constructed out of edible materials",
+  "Suitable for a romantic date night"
+
+];
+
+const personalInterests=[
+  "Makes you laugh out loud",
+  "Your parents would enjoy the experience",
+  "Uses technology that you’re unfamiliar with",
+  "Uses technology that you already own", 
+  "Similar to an existing experience you know about",
+  "You’re an animal-lover",
+  "You’re a nature-lover",
+  "You want the pitch with the shortest explanation", 
+  "You want the most expensive pitch",
+  "You want the most affordable pitch",
+  "You want an experience that looks good on social media",
+  "Easiest pitch for you to visualize in your mind", 
+  "You want high visitor throughput"  
+];
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+function randomTopic(){
+    var randomInt = getRandomInt(0, Object.keys(contentDic).length);
+    document.getElementById("pitchTopic1").innerHTML = contentDic[randomInt]["topic"];
+    document.getElementById("pitchTopic2").innerHTML = contentDic[randomInt]["topic"];
+    document.getElementById("pitchTopic3").innerHTML = contentDic[randomInt]["topic"];
+    document.getElementById("pitchTopic4").innerHTML = contentDic[randomInt]["topic"];
+    // if(topics_category[randomInt]=="Technology"){
+    //   document.getElementById("pitchTopic-img").src = "img/investor/topic_tech.png";
+    // }
+    // else if(topics_category[randomInt]=="Science"){
+    //   document.getElementById("pitchTopic-img").src = "img/investor/topic_sci.png";
+    // }
+    // else if(topics_category[randomInt]=="Diversity"){
+    //   document.getElementById("pitchTopic-img").src = "img/investor/topic_div.png";
+    // }
+    return randomInt;
+}
+
+function randomRefinement() {
+    var randomInt = getRandomInt(0, refinements.length);
+    document.getElementById("pitchRefinement1").innerHTML = refinements[randomInt];
+    document.getElementById("pitchRefinement2").innerHTML = refinements[randomInt];
+    document.getElementById("pitchRefinement3").innerHTML = refinements[randomInt];
+    document.getElementById("pitchRefinement4").innerHTML = refinements[randomInt];
+    return randomInt;
+}
+
+function randomPersonalInterest(){
+    var randomInt = getRandomInt(0, personalInterests.length);
+    document.getElementById("personalInterest1").innerHTML = personalInterests[randomInt];
+    // document.getElementById("personalInterest2").innerHTML = personalInterests[randomInt];
+    // document.getElementById("personalInterest3").innerHTML = personalInterests[randomInt];  
+}
+
+function randomContent(){
+  var topicNum = randomTopic();
+  var refinementNum = randomRefinement();
+  var topicCode = String.fromCharCode(65+topicNum);;
+  randomPersonalInterest();
+
+  document.getElementById("investorTopicCode").innerHTML = topicCode+refinementNum.toString();
+}
+
+function showTopic(){
+  var code1 = document.getElementById('topicCode').value.substring(0,1);
+  var code1Num = code1.charCodeAt(0)-65;
+  var code2 = parseInt(document.getElementById('topicCode').value.substring(1));
+  //Designer Page
+  document.getElementById("designTopic").innerHTML = contentDic[code1Num]["topic"];
+  document.getElementById("designRefinement").innerHTML = refinements[code2];
+  document.getElementById("checkDiv").style.display="none";
+  if("inspiration" in contentDic[code1Num]){
+    show(document.getElementById("inspirationBtn"));
+  }
+  show(document.getElementById("designTopicDiv"));
+  //Investor Page
+
+}
+
+function hintShow(){
+  var code1 = document.getElementById('topicCode').value.substring(0,1);
+  var code1Num = code1.charCodeAt(0)-65;
+  var inspirationTxt = document.getElementsByClassName('Inspiration-txt');
+  var inspirationImg = document.getElementsByClassName('Inspiration-img');
+  for (var i = 0; i < 4; i++) {
+    inspirationTxt[i].innerHTML = contentDic[code1Num]["inspiration"][i];
+    inspirationImg[i].src = contentDic[code1Num]["imgs"][i];
+  }
+  show(document.getElementById("inspirationHints"));
+
+}
+
+function hintClose(){
+  hide(document.getElementById("inspirationHints"));
+}
--- js/gamePage.js
+++ js/gamePage.js
@@ -49,14 +49,6 @@ function showInvestorPage2(){
 /* --------------------------------------------------------------
  # DesignerPage
 ---------------------------------------------------------------*/
-function hintShow(){
-  show(document.getElementById("inspirationHints"));
-}
-
-function hintClose(){
-  hide(document.getElementById("inspirationHints"));
-}
-
 
 /*--Timer----------------*/
 var second=30;
@@ -106,179 +98,10 @@ function showInvestorPage4(){
   show(document.getElementById("investorPage4"));  
 }
 
-
-
-/* --------------------------------------------------------------
- # Topic
----------------------------------------------------------------*/
-const topics = [
-"Use AR (Augmented Reality) for an educational experience",
-"Design an interactive experience on water resource protection",
-"Combine two different technologies for an interactive experience",
-"Design an experience based on your impression of Pittsburgh", 
-"Design an exhibit that can be easily updated yearly", 
-"Use gesture technology to create an experience on climate change", 
-"Design an experience based on a personal hobby", 
-"Use a mobile app in an unconventional way",  
-"Highlight women’s contributions to science and technology",  
-"Highlight minorities’ contributions to science and technology", 
-"Highlight a scientist who you believe deserves more recognition",  
-"Design an experience to reflect monthly astronomy discoveries", 
-"Combat health misinformation", 
-"Combat climate change misinformation", 
-"Create an interactive learning experience for large groups", 
-"Create an immersive experiential event", 
-"Create a VR (Virtual Reality) experience on the pandemic" 
-];
-
-//Technology, Science, Diversity
-const topics_category = [
-  "Technology",
-  "Science",
-  "Technology",
-  "Diversity",
-  "Technology",
-  "Technology",
-  "Diversity",
-  "Technology",
-  "Diversity",
-  "Diversity",
-  "Diversity",
-  "Science",
-  "Science", 
-  "Science",
-  "Diversity",
-  "Diversity",
-  "Technology"
-];
-
-
-const refinements = [
-  "Understandable for non-English speakers",
-  "Includes all age groups", 
-  "Accessible for physically disabled individuals",
-  "Easy to maintain social distance",
-  "Acts as a local landmark",
-  "Can be a traveling exhibit",
-  "Requires communication with other visitors",
-  "Integrates storytelling into the experience",
-  "Lets visitors become a permanent part of the experience",
-  "Lets visitors enjoy a personalized experience", 
-  "Improves visitors’ mental health",
-  "Costs 300 dollars to visit",
-  "Requires visitors to wear sports clothes",
-  "Opens only at night",
-  "Opens for only 1 hour a day",
-  "Located on top of a mountain",
-  "Located at a public transit stop",
-  "Involves pop culture (anime, superheroes, etc.)",
-  "Requires visitors to wear Halloween costumes", 
-  "Primarily targeted towards visitors’ pets", 
-  "Constructed out of edible materials",
-  "Suitable for a romantic date night"
-
-];
-
-const personalInterests=[
-  "Makes you laugh out loud",
-  "Your parents would enjoy the experience",
-  "Uses technology that you’re unfamiliar with",
-  "Uses technology that you already own", 
-  "Similar to an existing experience you know about",
-  "You’re an animal-lover",
-  "You’re a nature-lover",
-  "You want the pitch with the shortest explanation", 
-  "You want the most expensive pitch",
-  "You want the most affordable pitch",
-  "You want an experience that looks good on social media",
-  "Easiest pitch for you to visualize in your mind", 
-  "You want high visitor throughput"  
-];
-
-let inspirationDic = {
-  0:{
-      topic : "Use AR (Augmented Reality) for an educational experience",
-      category : "Technology",
-      inspiration : [
-        "eee",
-        "www",
-        "ddd"
-      ],
-    }, 
-  1:{
-      topic : "Use AR (Augmented Reality) for an educational experience",
-      category : "Technology",
-      inspiration : [
-        "aaa",
-        "bbb",
-        "ccc"
-      ],
-    },
-};
-
 function test1(){
   alert(inspirationDic[1]["inspiration"][0]);
 }
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
-
-function randomTopic(){
-    var randomInt = getRandomInt(0, topics.length);
-    document.getElementById("pitchTopic1").innerHTML = topics[randomInt];
-    document.getElementById("pitchTopic2").innerHTML = topics[randomInt];
-    document.getElementById("pitchTopic3").innerHTML = topics[randomInt];
-    document.getElementById("pitchTopic4").innerHTML = topics[randomInt];
-    // if(topics_category[randomInt]=="Technology"){
-    //   document.getElementById("pitchTopic-img").src = "img/investor/topic_tech.png";
-    // }
-    // else if(topics_category[randomInt]=="Science"){
-    //   document.getElementById("pitchTopic-img").src = "img/investor/topic_sci.png";
-    // }
-    // else if(topics_category[randomInt]=="Diversity"){
-    //   document.getElementById("pitchTopic-img").src = "img/investor/topic_div.png";
-    // }
-    return randomInt;
-}
-
-function randomRefinement() {
-    var randomInt = getRandomInt(0, refinements.length);
-    document.getElementById("pitchRefinement1").innerHTML = refinements[randomInt];
-    document.getElementById("pitchRefinement2").innerHTML = refinements[randomInt];
-    document.getElementById("pitchRefinement3").innerHTML = refinements[randomInt];
-    document.getElementById("pitchRefinement4").innerHTML = refinements[randomInt];
-    return randomInt;
-}
-
-function randomPersonalInterest(){
-    var randomInt = getRandomInt(0, personalInterests.length);
-    document.getElementById("personalInterest1").innerHTML = personalInterests[randomInt];
-    // document.getElementById("personalInterest2").innerHTML = personalInterests[randomInt];
-    // document.getElementById("personalInterest3").innerHTML = personalInterests[randomInt];  
-}
-
-function randomContent(){
-  var topicNum = randomTopic();
-  var refinementNum = randomRefinement();
-  var topicCode = String.fromCharCode(65+topicNum);;
-  randomPersonalInterest();
-
-  document.getElementById("investorTopicCode").innerHTML = topicCode+refinementNum.toString();
-}
-
-function showTopic(){
-  var code1 = document.getElementById('topicCode').value.substring(0,1);
-  var code1Num = code1.charCodeAt(0)-65;
-  var code2 = parseInt(document.getElementById('topicCode').value.substring(1));
-  //Designer Page
-  document.getElementById("designTopic").innerHTML = topics[code1Num];
-  document.getElementById("designRefinement").innerHTML = refinements[code2];
-  document.getElementById("checkDiv").style.display="none";
-  show(document.getElementById("designTopicDiv"));
-  //Investor Page
-
-}
 
 //Designer Page Enter to Submit//
 function  submitCode(){
@@ -291,6 +114,9 @@ function  submitCode(){
   });
 }
 
+/* --------------------------------------------------------------
+ # Sound
+---------------------------------------------------------------*/
 //Play onClick Sound
 function playButtonSound(){
   var audio = new Audio();
@@ -304,6 +130,5 @@ function playBGM(){
   audio.src="sound/BGM.mp3";
   audio.play();
 }
-
 
 
